b4e207be4055ff5f497200063f9cac14fb33bbdc
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-09 09:34:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,47 +7,34 @@ require 'pry'
 # items, returns an array containing all items that have been added
 # void_last_transaction, subtracts the last item from the total, returns the total to 0.0 if all items have been removed
 
-require 'pry'
-
 class CashRegister
-  attr_accessor :total, :discount, :quantity, :price
 
-  def initialize(discount = 0)
+  attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount=0)
     @total = 0
     @discount = discount
-    @price = price
-    @quantity = quantity
     @items = []
-    @transactions = []
   end
 
-  def add_item(title, price, quantity = 1)
-    @total += price * quantity
-    @transactions << price
-    i = quantity
-    until i == 0 do
-      @items << title
-      i -= 1
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
     end
+    self.last_transaction = amount * quantity
   end
 
   def apply_discount
-    if @discount == 0
-      "There is no discount to apply."
-    else
-      self.total -= (0.01 * @discount * @total).to_i
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
       "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
     end
   end
 
-  def items
-    @items
-  end
-
   def void_last_transaction
-    self.total = @total - @transactions.pop
-    if @items -= 0
-      return 0.0
-    end  
+    self.total = self.total - self.last_transaction
   end
 end
